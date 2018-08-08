5a257ddb670af25a8d8645097a7f9f7f5595692b
zhengzd <1280244614@qq.com>
2018-08-08 20:13:23 +0800

    Initial commit

---
+++ src/main/java/com/zzd/importdata/ImportDataApplication.java
@@ -0,0 +1,12 @@
+package com.zzd.importdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportDataApplication.class, args);
+	}
+}
